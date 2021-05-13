a89a6608751ff797f9e1559fd337d9d6e40cc3d9
Indra Saputra <flame.fire99@gmail.com>
2021-05-13 21:54:26 +0700

    feat: wrap all usecases and its dependencies to a main file

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,41 @@
 package main
 
-// let empty for now.
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/indrasaputra/sulong/internal/config"
+	"github.com/indrasaputra/sulong/internal/tool"
+	"github.com/indrasaputra/sulong/usecase"
+)
+
+const (
+	timeout = 5 * time.Second
+)
+
 func main() {
-	// main function will be filled once all the cores have been developed.
+	cfg, err := config.NewConfig(".env")
+	if err != nil {
+		log.Printf("fail initiate config: %v", err)
+		return
+	}
+
+	client := &http.Client{
+		Timeout: timeout,
+	}
+
+	getter := tool.NewTaniFundCrawler(client, cfg.TaniFund.URL)
+	notifier := tool.NewTelegramBot(client, cfg.Telegram.URL, cfg.Telegram.Token)
+
+	checker := usecase.NewTaniFundProjectChecker(getter, notifier, cfg.Telegram.RecipientID)
+
+	for {
+		if err := checker.CheckAndNotify(); err != nil {
+			log.Printf("check and notify: %v", err)
+		} else {
+			log.Printf("success check and notify")
+		}
+		time.Sleep(1 * time.Hour)
+	}
 }
